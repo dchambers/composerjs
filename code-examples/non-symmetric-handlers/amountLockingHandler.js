71d76da552e96e0592af714b807e26a59675292e
dchambers <dominic.chambers@gmail.com>
2015-01-28 14:44:31 +0000

    Simplified how amountLockingHandler is implemented, so that it no longer needs to rely on the 'current' or 'index' parameters.

--- code-examples/non-symmetric-handlers/amountLockingHandler.js
+++ code-examples/non-symmetric-handlers/amountLockingHandler.js
@@ -1,9 +1,12 @@
 'use strict';
 
 function amountLockingHandler(input, output, current, index) {
-  if(input.amountLocked) {
-    output.amount = current[0].amount;
+  if(input.globalAmount !== null) {
+    output.amount = input.globalAmount;
   }
 }
+
+amountLockingHandler.inputs = ['globalAmount'];
+amountLockingHandler.outputs = ['amount'];
 
 module.exports = amountLockingHandler;
